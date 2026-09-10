ef5774a5249de83d19c2c407e0c0eab0d8710abb
Indy Kavelaars <i.kavelaars@nijmegen.nl>
2026-09-10 16:40:26 +0200

    fix: test for login

--- src/app/home/test/HomeRequestHandler.test.ts
+++ src/app/home/test/HomeRequestHandler.test.ts
@@ -3,5 +3,5 @@ import { HomeRequestHandler } from '../HomeRequestHandler';
 test('zonder sessie stuurt home terug naar login', async () => {
   const response = await new HomeRequestHandler({ session: undefined }).handleRequest();
   expect(response.statusCode).toBe(302);
-  expect(response.headers?.Location).toBe('/gemachtigd/login');
+  expect(response.headers?.Location).toBe('/login');
 });
