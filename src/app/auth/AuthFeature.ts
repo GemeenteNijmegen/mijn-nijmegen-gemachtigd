86881c98b02289baf525694ef7ab8e2f1bc4eab9
Indy Kavelaars <i.kavelaars@nijmegen.nl>
2026-09-10 16:50:42 +0200

    fix: auth basepath

--- src/app/auth/AuthFeature.ts
+++ src/app/auth/AuthFeature.ts
@@ -5,11 +5,11 @@ import { Tracing } from 'aws-cdk-lib/aws-lambda';
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
-import { AuthFunction } from './auth-function';
 import { SessionsTable } from '../../infrastructure/SessionsTable';
 import { applyPageLambdaDefaults, createLambdaLogGroup } from '../../infrastructure/shared/PageLambda';
 import { Statics } from '../../Statics';
 import { LoginFunction } from '../login/login-function';
+import { AuthFunction } from './auth-function';
 import { VERID_CONFIG_ENV_VAR, VERID_SECRET_ARN_ENV_VAR } from './verid/VerIdConfiguration';
 
 interface AuthFeatureProps {
@@ -55,7 +55,7 @@ export class AuthFeature extends Construct {
     verIdSecret.grantRead(authFunction);
 
     props.httpApi.addRoutes({
-      path: '/login',
+      path: Statics.basePath + '/login',
       methods: [HttpMethod.GET],
       integration: new HttpLambdaIntegration('integration-login-function', loginFunction),
     });
